539284c627d1a01c98532649bd6f3e443033cda8
weisiqian <1535658549@qq.com>
2021-06-17 18:10:54 +0800

    init: 初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: '博客文档',
+  description: '博客文档博客文档博客文档博客文档',
+  base: '/',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+    nav:[
+      {
+        text: '博客',
+        link: '/page/guide/introduction'
+      },
+      {
+        text: 'Git',
+        items: [
+          { text: 'Git地址', link: 'https://github.com/weisiqian/blog' },
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        title: '文章列表',
+        collapsable: false,
+        children: [
+          ['/page/guide/introduction', '介绍'],
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+actionText: 快速查看 →
+actionLink: /page/guide/introduction
+features:
+- title: xxxxxx
+  details: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+- title: xxxxxx
+  details: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+- title: xxxxxx
+  details: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+footer: Copyright © 2021-present
+---
